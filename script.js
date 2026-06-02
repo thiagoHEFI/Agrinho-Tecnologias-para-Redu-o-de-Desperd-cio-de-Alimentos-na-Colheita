ae9512061b286bf0bb308f81f440585c23011ece
thiagoHEFI <thiago.fierakoski@escola.pr.gov.br>
2026-06-02 15:58:44 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+function mostrarMensagem(){
+
+document.getElementById("mensagem").innerHTML =
+"🌍 A FAO estima que bilhões de toneladas de alimentos são perdidas anualmente. A adoção de tecnologias agrícolas inteligentes é uma das estratégias mais importantes para aumentar a produção sem expandir áreas de cultivo.";
+}
